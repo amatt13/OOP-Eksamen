ec7d6e0b588cb88b754b82e0aea9433c56bb57ea
Helstedxd <fisseerskont@gmail.com>
2014-04-03 16:14:18 +0200

    asfd

--- OOP-Eksamen/Classer.cs
+++ OOP-Eksamen/Classer.cs
@@ -6,63 +6,130 @@ using System.Threading.Tasks;
 
 namespace OOP_Eksamen
 {
-    class Classer
+    public class Vehicle
     {
-        private class Vehicle 
+        private string _name;
+        private uint _km;
+        private string _registration;
+        private int _year;
+        private int _newPrice;
+        private bool _towHook;
+        private enum _licenseType { A, B, C, D, BE, CE, DE }
+        private double _engineSize;
+        private double _kmL;
+        private enum _fuel { Diesel, Gasoline }
+        private enum _energyClass { A, B, C, D }
+
+        public string Name
         {
-            string Name { get; set; }
-            int Km { get; set; }
-            string Registration { get; set; }
-            int Year { get; }
-            int NewPrice { get; set; }
-            bool TowHook { get; set; }
-            enum LicenseType { A, B, C, D, BE, CE, DE };
-            double EngineSize { get; set; }
-            double KmL { get; set; }
-            enum Fuel { Diesel, Gasoline }
-            enum EnergyClass { A, B, C, D }
+            get { return _name; }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Vehicle name not set", "NoVehicleName");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
         }
 
-        private class PersonVehicle : Vehicle {
-            int Seat { get; set; }
-            double[] Dimensions = new double[3]; // Højde, Lægnde, Bredde
-        }
-
-        public class PersonVehiclePrivate : PersonVehicle
+        public uint Km
         {
-            bool IsofixMount { get; set; }
+            get { return _km; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Kilometers is below 0", "KmTooLow");
+                }
+                else
+                {
+                    _km = value;
+                }
+            }
         }
 
-        public class PersonVehicleWork : PersonVehicle
+        public string Registration
         {
-            bool SafetyBar { get; set; }
-            int LoadCapacity { get; set; }
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
         }
 
-        public class Truck : Vehicle
+        public int Year
         {
-            int LoadCapacity { get; set; }
-            double Height { get; set; }
-            double Length { get; set; }
-            int Weight { get; set; }
-
+            get { return _year; }
+            set { throw new NotImplementedException(); }
         }
 
-        public class Bus : Vehicle
+        public int NewPrice
         {
-            int Seat { get; set; }
-            int Beds { get; set; }
-            bool Toilet { get; set; }
-            double Height { get; set; }
-            double Length { get; set; }
-            int Weight { get; set; }
+            get { return _newPrice; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _newPrice = 0;
+                }
+                else
+                {
+                    _newPrice = value;
+                }
+            }
         }
 
-        public class Camper : Vehicle {
-            int Seat { get; set; }
-            int Beds { get; set; }
-            bool Toilet { get; set; }
-            enum Heat { Gas, Electricity, Oil }
-        }
+
+
     }
+
+    /*
+    private class PersonVehicle : Vehicle
+    {
+        int Seat { get; set; }
+        double[] Dimensions = new double[3]; // Højde, Lægnde, Bredde
+    }
+
+    public class PersonVehiclePrivate : PersonVehicle
+    {
+        bool IsofixMount { get; set; }
+    }
+
+    public class PersonVehicleWork : PersonVehicle
+    {
+        bool SafetyBar { get; set; }
+        int LoadCapacity { get; set; }
+    }
+
+    public class Truck : Vehicle
+    {
+        int LoadCapacity { get; set; }
+        double Height { get; set; }
+        double Length { get; set; }
+        int Weight { get; set; }
+
+    }
+
+    public class Bus : Vehicle
+    {
+        int Seat { get; set; }
+        int Beds { get; set; }
+        bool Toilet { get; set; }
+        double Height { get; set; }
+        double Length { get; set; }
+        int Weight { get; set; }
+    }
+
+    public class Camper : Vehicle
+    {
+        int Seat { get; set; }
+        int Beds { get; set; }
+        bool Toilet { get; set; }
+        enum Heat { Gas, Electricity, Oil }
+    }
+    */
 }
--- OOP-Eksamen/Program.cs
+++ OOP-Eksamen/Program.cs
@@ -10,7 +10,13 @@ namespace OOP_Eksamen
     {
         static void Main(string[] args)
         {
-            
+            Vehicle bla = new Vehicle();
+            bla.Name = "Test Bil";
+            bla.Km = 0;
+            //bla.Registration = "AS1234";
+            bla.NewPrice = -1;
+
+            Console.WriteLine("Name: {0}, Kilometer: {1}, Price: {2}", bla.Name, bla.Km, bla.NewPrice);
 
             Console.ReadLine();
         }
