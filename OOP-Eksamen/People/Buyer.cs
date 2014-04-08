6b6337348107260c01044ae232157d71fecf9332
Helstedxd <fisseerskont@gmail.com>
2014-04-08 20:31:28 +0200

    Buyer & Salesman

--- OOP-Eksamen/People/Buyer.cs
+++ OOP-Eksamen/People/Buyer.cs
@@ -21,16 +21,10 @@ namespace OOP_Eksamen
         }
 
         //The constructor is the place where the account balance is set.
-        public Buyer(decimal Balance, decimal Credit)
-        {
-            _balance = Balance;
-            _credits = Credit;
-        }
-
-        public abstract bool RemoveBalance();
+        public Buyer() { }
     }
 
-    class Business : Buyer
+    class BusinessBuyer : Buyer
     {
         private int _cvr;
 
@@ -57,6 +51,50 @@ namespace OOP_Eksamen
             {
                 return false;
             }
+        }
+
+        public BusinessBuyer(decimal Balance, decimal Credit, int CVR)
+        {
+            _balance = Balance;
+            _credits = Credit;
+            _cvr = CVR;
+        }
+    }
+
+    class PrivateBuyer : Buyer
+    {
+        private int _cpr;
+
+        public int CPR
+        {
+            get
+            {
+                return _cpr;
+            }
+
+            set
+            {
+                _cpr = value;
+            }
+        }
+
+        public bool RemoveBalance(decimal Price)
+        {
+            if (Price > _balance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public PrivateBuyer(decimal Balance, decimal Credit, int CPR)
+        {
+            _balance = Balance;
+            _credits = 0;
+            _cpr = CPR;
         }
     }
 }
--- OOP-Eksamen/People/Seller.cs
+++ OOP-Eksamen/People/Seller.cs
@@ -8,9 +8,9 @@ namespace OOP_Eksamen
 {
     class Seller
     {
-        private decimal _balance;
-        private int _zipCode;
-        private bool _notification;
+        protected decimal _balance;
+        protected int _zipCode;
+        protected bool _notification;
 
         public decimal Balance
         {
@@ -43,6 +43,64 @@ namespace OOP_Eksamen
             Console.WriteLine("OMG I GOT BID, SUCH PRICE!!!");
             return true;
         }
-
     }
+
+    class BusinessSeller : Seller
+    {
+        private int _cvr;
+
+        public int CVR
+        {
+            get
+            {
+                return _cvr;
+            }
+
+            set
+            {
+                _cvr = value;
+            }
+        }
+
+        public void AddBalance(decimal Price)
+        {
+            _balance += Price;
+        }
+
+        public BusinessSeller(decimal Balance, int CVR)
+        {
+            _balance = Balance;
+            _cvr = CVR;
+        }
+    }
+
+    class PrivateSeller : Seller
+    {
+        private int _cpr;
+
+        public int CPR
+        {
+            get
+            {
+                return _cpr;
+            }
+
+            set
+            {
+                _cpr = value;
+            }
+        }
+
+        public void AddBalance(decimal Price)
+        {
+            _balance += Price;
+        }
+
+        public PrivateSeller(decimal Balance, int CPR)
+        {
+            _balance = Balance;
+            _cpr = CPR;
+        }
+    }
+
 }
