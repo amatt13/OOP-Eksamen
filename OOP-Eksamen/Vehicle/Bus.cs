42836a28a6bff5038a16e9645be5cdb3a17322f8
amatt13 <anders.matthiassen@gmail.com>
2014-04-08 20:42:11 +0200

    Startup

--- OOP-Eksamen/Vehicle/Bus.cs
+++ OOP-Eksamen/Vehicle/Bus.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace OOP_Eksamen
 {
-    class Bus
+    class Bus : Vehicle
     {
+        private uint _seat;
+        private uint _beds;
+        private bool _toilet;
+        private Size _sizeOfBus;
+        private uint _weight;
+
+        private uint Seat {
+            get{
+                return _seat;
+            }
+            set {
+                if (value > 0) {
+                    _seat = value;
+                }
+                else {
+                    throw new ArgumentException("There have to be atleast 1 seat in a bus", "SeatTooLow");
+                }
+            }
+        }
+
+        private uint Beds {
+            get {
+                return _beds;
+            }
+            set {
+                if (value < 999) {
+                    _beds = value;
+                }
+                else {
+                    throw new ArgumentException("The number of beds can't be a negative number", "BedTooLow");
+                }
+            }
+        }
+        private bool Toilet {
+            get {
+                return _toilet;
+            }//Er det her nødvendigt? -Anders
+            set {
+                _toilet = value;
+            }
+        }
+        private Size SizeOfBus { get; set; }
+        private uint Weight { get; set; }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
