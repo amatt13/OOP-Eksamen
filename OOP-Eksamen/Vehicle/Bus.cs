92e065e65a16c4f404ef2d2d8b4fc24480592ef8
amatt13 <anders.matthiassen@gmail.com>
2014-04-11 12:38:59 +0200

    Rettelser.

--- OOP-Eksamen/Vehicle/Bus.cs
+++ OOP-Eksamen/Vehicle/Bus.cs
@@ -10,46 +10,36 @@ namespace OOP_Eksamen
     {
         private uint _seat;
         private uint _beds;
-        private bool Toilet { get; set; }
-        private bool Hook { get; set; }
+        public bool Toilet { get; set; }
+        public bool Hook { get; set; }
         private Size _sizeOfBus;
         private double _weight;
         private LicenseType _license;
 
-        public uint Seat
-        {
-            get
-            {
+        public uint Seat{
+            get{
                 return _seat;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0){
                     _seat = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("There have to be atleast 1 seat in a bus", "SeatTooLow");
                 }
             }
         }
 
-        public uint Beds
-        {
-            get
-            {
+        public uint Beds{
+            get{
                 return _beds;
             }
-            set
-            {
-                if (value < 999)
-                {
+            set{
+                if (value < 999){
                     _beds = value;
                 }
-                else
-                {
-                    throw new ArgumentException("The number of beds can't be a negative number", "BedTooLow");
+                else{
+                    throw new ArgumentException("There have been placed 1.000 beds or more.\nWas this a mistake?", "BedTooHigh");
                 }
             }
         }
@@ -62,66 +52,49 @@ namespace OOP_Eksamen
                  _toilet = value;
              }
          }*/
-        public Size SizeOfBus
-        {
-            get
-            {
+        public Size SizeOfBus{
+            get{
                 return _sizeOfBus;
             }
-            set
-            {
-                if (_sizeOfBus.Depth > 0 || _sizeOfBus.Height > 0 || _sizeOfBus.Width > 0)
-                {
+            set{
+                if (_sizeOfBus.Depth > 0 || _sizeOfBus.Height > 0 || _sizeOfBus.Width > 0){
                     _sizeOfBus = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("One or more of these following values were below zero: Depth, Height and/or Width", "SizeToLow");
                 }
             }
         }
 
-        public double Weight
-        {
-            get
-            {
+        public double Weight{
+            get{
                 return _weight;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0){
                     _weight = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("The bus weights too little", "WeightTooLow");
                 }
             }
         }
 
-        private LicenseType Licence
-        {
-            get
-            {
+        public LicenseType Licence{
+            get{
                 return _license;
             }
-            set
-            {
-                if (Hook == true)
-                {
+            set{
+                if (Hook == true){
                     value = LicenseType.DE;
                 }
-                else
-                {
+                else{
                     value = LicenseType.D;
                 }
-
             }
         }
 
-        public override string ToString()
-        {
+        public override string ToString(){
 
             string returnString;
             string Sseat = Seat.ToString();
@@ -132,7 +105,7 @@ namespace OOP_Eksamen
             string SsizeOfBusDepth = SizeOfBus.Depth.ToString();
             string SsizeOfBusWidth = SizeOfBus.Width.ToString();
             string Sweight = Weight.ToString();
-
+            
             returnString = "This bus have " + Sseat + " seats, " + Sbeds + " beds, " + " and it is " + Stoilet + " that it contains a toilet, while it is " + Shook + " the bus have a hook. The dimentions is " + SsizeOfBusHeight + "m height, " + SsizeOfBusDepth + "m deep and " + SsizeOfBusWidth + "m width. The weight is " + Sweight + "kg.";
 
             return returnString;
