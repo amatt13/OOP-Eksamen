64058a15f9407bd7ccf8f2443021ea41669a193d
AimedZERO <Josias.Laugesen@gmail.com>
2014-04-14 19:54:52 +0200

    Slettede en stub

--- OOP-Eksamen/Vehicle/PersonVehiclePrivate.cs
+++ OOP-Eksamen/Vehicle/PersonVehiclePrivate.cs
@@ -9,25 +9,12 @@ namespace OOP_Eksamen
     class PersonVehiclePrivate : PersonVehicle
     {
         private bool _isofix;
-        private string p1;
-        private int p2;
-        private string p3;
-        private int p4;
-        private int p5;
-        private bool p6;
-        private int p7;
-        private FuelType fuelType;
-        private int p8;
-        private int p9;
-        private Size sizePv1;
 
         public bool Isofix
         {
             get { return _isofix; }
             set { _isofix = value; }
         }
-
-
 
         public PersonVehiclePrivate(string name, double km, string reg, int year, decimal newPrice, bool towHook, double kmPerLiter, FuelType fuelType, decimal minPrice, uint seats, Size sizeOfPv, bool isofix)
             : base(name, km, reg, year, newPrice, towHook, kmPerLiter, fuelType, minPrice, seats, sizeOfPv)
