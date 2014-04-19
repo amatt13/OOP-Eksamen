f1fede72cc33443c78a942c3e08e53c621ad8ca1
Helstedxd <fisseerskont@gmail.com>
2014-04-19 20:12:50 +0200

    formatering

--- OOP-Eksamen/AuctionHouse.cs
+++ OOP-Eksamen/AuctionHouse.cs
@@ -4,90 +4,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Eksamen {
+namespace OOP_Eksamen
+{
 
-struct VehicleBids {
-    public Buyer PlacedBy;
-    public decimal Bid;
-    public int AuctionNumber;
-    public DateTime BidPlaced;
-}
-
-class AuctionHouse {
-
-    public List<Seller> Sellers = new List<Seller>();
-    public List<Buyer> Buyers = new List<Buyer>();
-
-    public List<Vehicle> VehiclesSold = new List<Vehicle>();
-    public List<Vehicle> VehiclesForSale = new List<Vehicle>();
-    public List<VehicleBids> Bids = new List<VehicleBids>();
-
-    CalculateFees FeeCalculater = new CalculateFees();
-
-    private int i = 0;
-
-    public delegate void NotificationMethod(Vehicle k);
-
-    public int PutOpForSale(Vehicle v, Seller s, decimal MinPrice, NotificationMethod Method) {
-        v.handler += Method;
-        VehiclesForSale.Add(v);
-        s.Vehicles.Add(v);
-        v.MinPrice = MinPrice;
-        v.Seller = s;
-        v.AuctionNumber = i;
-        return i++;
+    struct VehicleBids
+    {
+        public Buyer PlacedBy;
+        public decimal Bid;
+        public int AuctionNumber;
+        public DateTime BidPlaced;
     }
 
-    public bool ReciveOffer(Buyer Buyer, int AuctionNumber, decimal Offer) {
-        if (AuctionNumber == -1) {
-            throw new ArgumentException("The vehicle has not been registered in the auction house", "NotRegisteredAuctionHouse");
+    class AuctionHouse
+    {
+
+        public List<Seller> Sellers = new List<Seller>();
+        public List<Buyer> Buyers = new List<Buyer>();
+
+        public List<Vehicle> VehiclesSold = new List<Vehicle>();
+        public List<Vehicle> VehiclesForSale = new List<Vehicle>();
+        public List<VehicleBids> Bids = new List<VehicleBids>();
+
+        CalculateFees FeeCalculater = new CalculateFees();
+
+        private int i = 0;
+
+        public delegate void NotificationMethod(Vehicle k);
+
+        public int PutOpForSale(Vehicle v, Seller s, decimal MinPrice, NotificationMethod Method)
+        {
+            v.handler += Method;
+            VehiclesForSale.Add(v);
+            s.Vehicles.Add(v);
+            v.MinPrice = MinPrice;
+            v.Seller = s;
+            v.AuctionNumber = i;
+            return i++;
         }
 
-        IEnumerable<Vehicle> Vehicle = VehiclesForSale.Where(v => v.AuctionNumber == AuctionNumber).Take(1);
+        public bool ReciveOffer(Buyer Buyer, int AuctionNumber, decimal Offer)
+        {
+            if (AuctionNumber == -1)
+            {
+                throw new ArgumentException("The vehicle has not been registered in the auction house", "NotRegisteredAuctionHouse");
+            }
 
-        Vehicle.First().handler(Vehicle.First());
+            IEnumerable<Vehicle> Vehicle = VehiclesForSale.Where(v => v.AuctionNumber == AuctionNumber).Take(1);
 
-        if (Buyer.Balance >= Offer && Vehicle.First().MinPrice <= Offer) {
-            VehicleBids Bid = new VehicleBids();
-            Bid.Bid = Offer;
-            Bid.PlacedBy = Buyer;
-            Bid.AuctionNumber = AuctionNumber;
-            Bid.BidPlaced = DateTime.Now;
+            Vehicle.First().handler(Vehicle.First());
 
-            Vehicle.First().MinPrice = Offer;
+            if (Buyer.Balance >= Offer && Vehicle.First().MinPrice <= Offer)
+            {
+                VehicleBids Bid = new VehicleBids();
+                Bid.Bid = Offer;
+                Bid.PlacedBy = Buyer;
+                Bid.AuctionNumber = AuctionNumber;
+                Bid.BidPlaced = DateTime.Now;
 
-            Bids.Add(Bid);
+                Vehicle.First().MinPrice = Offer;
 
+                Bids.Add(Bid);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool AcceptBid(Seller seller, int auctionNumber)
+        {
+            IEnumerable<Vehicle> vehicle = seller.Vehicles.Where(v => v.AuctionNumber == auctionNumber).Take(1);
+            decimal tmpPrice = vehicle.First().MinPrice;
+
+            if (vehicle == null || !vehicle.Any())
+            {
+                return false;
+            }
+
+            IEnumerable<VehicleBids> vehicleBids = Bids.Where(b => b.AuctionNumber == auctionNumber).OrderByDescending(b => b.BidPlaced).Take(1);
+
+            foreach (VehicleBids b in vehicleBids)
+            {
+                b.PlacedBy.RemoveBalance(tmpPrice);
+            }
+
+            tmpPrice -= FeeCalculater.Fees(tmpPrice);
+
+            seller.AddBalance(tmpPrice);
+
+            VehiclesSold.Add(vehicle.First());
+            VehiclesForSale.Remove(vehicle.First());
+            seller.Vehicles.Remove(vehicle.First());
             return true;
-        } else {
-            return false;
+
         }
     }
-
-
-    public bool AcceptBid(Seller seller, int auctionNumber) {
-        IEnumerable<Vehicle> vehicle = seller.Vehicles.Where(v => v.AuctionNumber == auctionNumber).Take(1);
-        decimal tmpPrice = vehicle.First().MinPrice;
-
-        if (vehicle == null || !vehicle.Any()) {
-            return false;
-        }
-
-        IEnumerable<VehicleBids> vehicleBids = Bids.Where(b => b.AuctionNumber == auctionNumber).OrderByDescending( b => b.BidPlaced ).Take(1);
-
-        foreach (VehicleBids b in vehicleBids) {
-            b.PlacedBy.RemoveBalance(tmpPrice);
-        }
-
-        tmpPrice -= FeeCalculater.Fees(tmpPrice);
-
-        seller.AddBalance(tmpPrice);
-
-        VehiclesSold.Add(vehicle.First());
-        VehiclesForSale.Remove(vehicle.First());
-		seller.Vehicles.Remove (vehicle.First ());
-        return true;
-
-    }
-}
 }
