4413ccac0c027066ecba5bdb554f0a7dd2540f42
amatt13 <anders.matthiassen@gmail.com>
2014-04-21 13:29:47 +0200

    Comments

--- OOP-Eksamen/Vehicle/PersonVehicle.cs
+++ OOP-Eksamen/Vehicle/PersonVehicle.cs
@@ -6,74 +6,55 @@ using System.Threading.Tasks;
 
 namespace OOP_Eksamen
 {
-    abstract class PersonVehicle : Vehicle
-    {
+    abstract class PersonVehicle : Vehicle{
         private uint _seat;
         private Size _trunk;
-        //Licenceype bliver håndteret i Work og Private.
+        //Licencetype will be handled in car and van.
 
-        public uint Seat
-        {
-            get
-            {
+        public uint Seat{
+            get{
                 return _seat;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0) {//An exception will be trown if the seats is set to 0, negative number or a NAN
                     _seat = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("There have to be atleast 1 seat in a vehicle", "SeatTooLow");
                 }
             }
         }
 
-        public Size Trunk
-        {
-            get
-            {
+        public Size Trunk{
+            get{
                 return _trunk;
             }
-            set
-            {
-                if (value.Depth > 0 && value.Height > 0 && value.Width > 0)
-                {
+            set{
+                if (value.Depth > 0 && value.Height > 0 && value.Width > 0){
                     _trunk = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("One or more of these following values were below zero: Depth, Height and/or Width", "SizeToLow");
                 }
             }
         }
 
-        public override double EngineSize
-        {
-            get
-            {
+        public override double EngineSize{
+            get{
                 return _engineSize;
             }
-
-            set
-            {
-                if (value >= 0.7 && value <= 10)
-                {
+            set{
+                if (value >= 0.7 && value <= 10){
                     _engineSize = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("Engine size not within engine size range", "NotInEngineSizeRange");
                 }
             }
         }
 
-
 		public PersonVehicle(string name, double km, string reg, int year, decimal newPrice, bool towHook, double kmPerLiter, FuelType fuelType, decimal minPrice, uint seats, Size size, double engineSize)
-			: base(name, km, reg, year, newPrice, towHook, kmPerLiter, fuelType, minPrice)
-        {
+			: base(name, km, reg, year, newPrice, towHook, kmPerLiter, fuelType, minPrice){
             Seat = seats;
             Trunk = size;
 			EngineSize = engineSize;
--- OOP-Eksamen/Vehicle/Truck.cs
+++ OOP-Eksamen/Vehicle/Truck.cs
@@ -4,122 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Eksamen
-{
-    class Truck : Vehicle
-    {
-        public uint LoadCapacity { get; set; }
+namespace OOP_Eksamen{
+    class Truck : Vehicle{
+        public uint LoadCapacity { get; set; } //Can't be negative since it is a unsigned int. 
         private double _height;
         private double _length;
         private double _weight;
         private LicenseType _license;
 
 
-        public double Weight
-        {
-            get
-            {
+        public double Weight{
+            get{
                 return _weight;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0){
                     _weight = value;
                 }
-                else
-                {
+                else{//An exception will be trown if the weight is set to 0, negative number or a NAN
                     throw new ArgumentException("The truck weights too little", "WeightTooLow");
                 }
             }
         }
 
-        public double Height
-        {
-            get
-            {
+        public double Height{
+            get{
                 return _height;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0){
                     _height = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("The truck can not have a negative height value", "HeightTooLow");
                 }
             }
         }
 
-        public double Length
-        {
-            get
-            {
+        public double Length{
+            get{
                 return _length;
             }
-            set
-            {
-                if (value > 0)
-                {
+            set{
+                if (value > 0){
                     _length = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("The truck can not have a negative lenght value", "LenghtTooLow");
                 }
             }
         }
 
-        public LicenseType License
-        {
-            get
-            {
+        public LicenseType License{
+            get{
                 return _license;
             }
-            set
-            {
-                if (_towHook == true)
-                {
+            set {//The licence is CE if the truck has towhook, otherwise a C licence is required
+                if (_towHook == true){
                     _license = LicenseType.CE;
                 }
-                else
-                {
+                else{
                     _license = LicenseType.C;
                 }
             }
         }
 
-        public override double EngineSize
-        {
-            get
-            {
+        public override double EngineSize{
+            get{
                 return _engineSize;
             }
 
-            set
-            {
-                if (value >= 4.2 && value <= 15)
-                {
+            set{//There is min and max limit the value has to be inbetween. Otherwise an exception will be thrown
+                if (value >= 4.2 && value <= 15){
                     _engineSize = value;
                 }
-                else
-                {
+                else{
                     throw new ArgumentException("Engine size not within engine size range", "NotInEngineSizeRange");
                 }
             }
         }
 
         public Truck()
-            : base("test", 1234, "AZ23412", 1999, 0, false, 0, Vehicle.FuelType.Gasoline, 0)
-        {
+            : base("test", 1234, "AZ23412", 1999, 0, false, 0, Vehicle.FuelType.Gasoline, 0){
             Length = 1;
         }
 
 		public Truck(string name, double km, string reg, int year, decimal newPrice, bool towHook, double kmPerLiter, FuelType fuelType, decimal minPrice, uint loadcapacity, double height, double weight, double length, double engineSize)
-			: base(name, km, reg, year, newPrice, towHook, kmPerLiter, fuelType, minPrice)
-        {
+			: base(name, km, reg, year, newPrice, towHook, kmPerLiter, fuelType, minPrice){
             LoadCapacity = loadcapacity;
             Height = height;
             Weight = weight;
@@ -128,8 +99,7 @@ namespace OOP_Eksamen
 			EngineSize = engineSize;
         }
 
-        public override string ToString()
-        {
+        public override string ToString(){
             return
                 string.Format("\n---Name: {0} \n---Type: Truck \n---Kilometers: {1} \n---Registration: {2} \n---Year: {3} \n---New Price: {4:#,0} DKK \n---Tow Hook: {5} \n---Engine Size: {6:0.0}L \n---Kilometers Per Liter: {7:0.0} Km/L \n---Fuel Type: {8} \n---Energyclass: {9} \n---Required Drivers License: {10} \n---Minimum Price: {11:#,0} DKK \n---LoadCapacity: {12} \n---Height: {13}\n---Length: {14}\n---Weight: {15}",
                     Name,
